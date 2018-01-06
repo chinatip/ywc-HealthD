09459c440686e95bbd90ca8c22a66aa7fa3fc66a
young <young@PIM.AC.TH>
2018-01-06 22:50:36 +0700

    Update mission

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -3,15 +3,49 @@ import styled from 'styled-components';
 import map from 'lodash.map';
 
 import Container from './BaseComponent/Container';
+import { Button } from './BaseComponent/Button';
 
 const MissionContainer = styled.div`
+  display: flex;
+`;
 
+const WelcomeWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 7px;
+
+`;
+const Header = styled.div`
+  font-size: 30px;
+  line-height: 50px;
+  font-weight: 700;
+  color: #de653b;
+`;
+const SubHeader = styled.div`
+  width: 410px;
+  font-size: 16px;
+  color: #848181f7;
+  flex: 1;
+`;
+const Condition = styled.div`
+  width: 410px;
+`;
+
+const MissionList = styled.div`
+  flex: 1;
 `;
 const CardContainer = styled.div`
   display: flex;
+  width: 95%;
   text-align: left;
+  padding: 10px;
+  margin: 0 2.5%;
   margin-bottom: 10px;
-  background: red;
+  border-radius: 4px;
+  border: 1px solid #cccccc45;
+  box-shadow: 5px 2px 12px -2px #ccc;
 
   img {
     width: 60px;
@@ -23,15 +57,27 @@ const CardContainer = styled.div`
 const MissionItemWrapper = styled.div`
 
 `;
+const Title = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  color: #504c4cf7;
+`;
+const Subtitle = styled.div`
+  font-size: 11px;
+  letter-spacing: 0.5px;
+  color: #848181f7;
+`;
 
 const MissionItem = ({ name, detail }) => {
   return (
     <CardContainer>
       <img />
       <MissionItemWrapper>
-        <div>{name}</div>
-        <div>{detail}</div>
+        <Title>{name}</Title>
+        <Subtitle>{detail}</Subtitle>
       </MissionItemWrapper>
+      <Button value="เข้าร่วม" />
     </CardContainer>
   );
 };
@@ -42,9 +88,18 @@ class Mission extends Component {
 
     return (
       <Container header={'Mission'}>
-        {map(missions, (m) => {
-          return <MissionItem name={m.name} detail={m.detail}/>;
-        })}
+        <MissionContainer>
+          <WelcomeWrapper>
+            <Header>ภารกิจประจำสัปดาห์</Header>
+            <SubHeader>ในสัปดาห์นี้ ภารกิจที่คุณสามารถเลือกทำได้ มีทั้งหมด 3 ภารกิจ ได้แก่...</SubHeader>
+            <Condition>* เงื่อนไขการปฏิบัติภารกิจ: แต่ละภารกิจมีอายุ 1 สัปดาห์ (เริ่มต้นวันจันทร์ สิ้นสุดวันอาทิตย์) หากยืนยันภารกิจได้ไม่ครบถ้วนภายในเวลา จะถือว่าภารกิจนั้นไม่สำเร็จ และจะไม่ได้รับคะแนนของภารกิจดังกล่าว</Condition>
+          </WelcomeWrapper>
+          <MissionList>
+            { map(missions, (m) => {
+              return <MissionItem name={m.name} detail={m.detail}/>;
+            })}
+          </MissionList>
+        </MissionContainer>
       </Container>
     );
   }
