9f0886337c0bafb64e695a52d8b4a1c944878aa9
young <young@PIM.AC.TH>
2018-01-06 09:36:45 +0700

    Add router and components file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+
+import Home from './components/Home'
+import Profile from './components/Profile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/profile" component={Profile} />
+          </div>
+        </Router>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
